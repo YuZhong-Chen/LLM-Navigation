d5854ce9fe62d649aa63ce78fd5de3bd0eeb46dc
YuZhong-Chen <yuzhong1214@gmail.com>
2024-09-23 03:24:43 +0800

    feat(docs_ws): Install sphinx_copybutton in Dockerfile.

--- docs_ws/src/conf.py
+++ docs_ws/src/conf.py
@@ -13,7 +13,10 @@ author = 'YuZhong-Chen'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_copybutton'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
